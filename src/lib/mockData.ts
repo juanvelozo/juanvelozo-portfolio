1580bd56ef72d430a12c9535647769aabcb7cde3
Juan Velozo <juanvelozomoreno18@gmail.com>
2023-09-22 14:30:36 -0300

    🚨fix(): fixed pipeline - removed dependency conflict with import on mockData.ts

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -1,7 +1,4 @@
 import {createElement} from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 // import corpcommentImg from "@/public/corpcomment.png";
 // import rmtdevImg from "@/public/rmtdev.png";
 // import wordanalyticsImg from "@/public/wordanalytics.png";
@@ -39,7 +36,6 @@ export const experiencesData = [
     location: "Miami, FL",
     description:
       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: createElement(LuGraduationCap),
     date: "2019",
   },
   {
@@ -47,7 +43,6 @@ export const experiencesData = [
     location: "Orlando, FL",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: createElement(CgWorkAlt),
     date: "2019 - 2021",
   },
   {
@@ -55,7 +50,6 @@ export const experiencesData = [
     location: "Houston, TX",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: createElement(FaReact),
     date: "2021 - present",
   },
 ] as const;
